56d0e4715f1dd0ece7441881cbcf474f5daef3df
PioKl <kosiu342@gmail.com>
2025-05-14 21:30:56 +0200

    Add ProductList and sampleData

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,20 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 export const metadata = {
   title: "Home",
 };
+
 const Homepage = () => {
-  return <h1>Prostore</h1>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
