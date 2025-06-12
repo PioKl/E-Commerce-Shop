c13ac4617674fa1bb5e698c401831a8c41356163
PioKl <kosiu342@gmail.com>
2025-06-12 19:15:05 +0200

    bug update

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -306,7 +306,7 @@ export async function getOrderSummary() {
   //Get monthy sales
   const salesDataRaw = await prisma.$queryRaw<
     Array<{ month: string; totalSales: Prisma.Decimal }>
-  >`SELECT to_char("createdAt", 'MM/YY') as "month", sum("totalPrice) as "totalSales" FROM "Order" GROUP BY to_char("createdAt", 'MM/YY')`;
+  >`SELECT to_char("createdAt", 'MM/YY') as "month", sum("totalPrice") as "totalSales" FROM "Order" GROUP BY to_char("createdAt", 'MM/YY')`;
 
   const salesData: SalesDataType = salesDataRaw.map((entry) => ({
     month: entry.month,
