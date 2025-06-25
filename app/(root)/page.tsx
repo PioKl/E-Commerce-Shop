1944948015a483f2acae4ab0679f48433f2597c8
PioKl <kosiu342@gmail.com>
2025-06-25 13:30:30 +0200

    Add ProductCarousel with featuredProducts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 export const metadata = {
   title: "Home",
@@ -7,8 +11,12 @@ export const metadata = {
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </>
   );
